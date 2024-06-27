51cb940939d16b4a9445124b3db969b77438d885
NixonR <nixonramosdue@gmail.com>
2024-06-27 00:12:11 -0500

    showing products

--- src/components/containerProducts.js
+++ src/components/containerProducts.js
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react"
 import menuIcon from "../resources/images/menu-vertical.svg"
+import { AUTHOR_ID, BASE_URL } from "../utils/consts"
 export const ContainerProducts = () =>{
+    const [dataProduct, setDataProduct] = useState([])
+
+    useEffect(()=>{
+        fetch(
+            BASE_URL+"/bp/products",
+            {
+                method:"GET",
+                headers: {   
+                    "authorId":AUTHOR_ID,
+                    "Access-Control-Allow-Origin":"*",
+                    "content-type":"application/json"
+                }
+            }).then(
+                res=>res.json()
+            ).then(data=>{
+                console.log("ok")
+                setDataProduct(data)
+        });
+    },[])
+
+
 
     return(
 
         <>
             <div className=" w-full h-full p-10 relative shadow-xl bg-white">
-                <div className="w-full h-full bg-white shadow-2xl">
+                <div className="w-full h-full bg-white shadow-2xl max-h-[500px] overflow-y-auto">
                     <table class=" w-full text-center">
                         <thead className="">
                             <tr className=" bg-gray-200 h-14 text-lg">
@@ -17,47 +40,36 @@ export const ContainerProducts = () =>{
                                 <th></th>
                             </tr>
                         </thead>
-                        <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Juan Pérez</td>
-                            <td>30</td>
-                            <td>juan@example.com</td>
-                            <td>juan@example.com</td>
-                            <td className="p-5">
-                                <div className="rotate-90 hover:cursor-pointer">
-                                    <span className="text-[5px] ">⚫⚫⚫</span>
-                                </div>
-                            </td>
-                        </tr>
+                        <tbody className="overflow-y-auto">
+                            {
+                                dataProduct.length>0?
+                                    dataProduct.map((item,idx)=>{
+                                        return(
+                                            <tr className=" border-b-2">
+                                                <td className=" flex h-20">
+                                                    <img src={item.logo} width={60} height={20} className="m-auto"/>
+                                                </td>
+                                                <td>{item.name}</td>
+                                                <td>{item.description}</td>
+                                                <td>{item.date_release.split("T")[0]}</td>
+                                                <td>{item.date_revision.split("T")[0]}</td>
+                                                <td className="p-5">
+                                                    <div className="rotate-90 hover:cursor-pointer">
+                                                        <span className="text-[5px] ">⚫⚫⚫</span>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                :
+                                <div><p>Nothing to show</p></div>
+                            }
                         
-                        <tr>
-                            <td>1</td>
-                            <td>Juan Pérez</td>
-                            <td>30</td>
-                            <td>juan@example.com</td>
-                            <td>juan@example.com</td>
-                            <td className="p-5">
-                                <div className="rotate-90 hover:cursor-pointer">
-                                    <span className="text-[5px] ">⚫⚫⚫</span>
-                                </div>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>1</td>
-                            <td>Juan Pérez</td>
-                            <td>30</td>
-                            <td>juan@example.com</td>
-                            <td>juan@example.com</td>
-                            <td className="p-5">
-                                <div className="rotate-90 hover:cursor-pointer">
-                                    <span className="text-[5px] ">⚫⚫⚫</span>
-                                </div>
-                            </td>
-                        </tr>
                         </tbody>
                     </table>
+                </div>
+                <div className="mt-10">
+                    <p>{dataProduct.length} Resultados</p>
                 </div>
             </div>
         </>
